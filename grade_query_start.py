cd8804e29b89d07ac1d8e07295b66bb8430944ed
Grady Coffey <gcoffey0744@floridapoly.edu>
2026-02-06 05:15:55 -0500

    Update grade_query_start.py
    I should sleep better

--- grade_query_start.py
+++ grade_query_start.py
@@ -3,52 +3,72 @@
 #
 
 # Use a dictionary named 'grades' to track student grades.
-# code here
- 
+grades = {}
 
-  # Loop until the user chooses to quit.
-  # Check user input for the following "(A)dd, (R)emove, (M)odify, (P)rint all, or (Q)uit? "
-  # Prevent unexected inputs by converting input to upper-case
-  # code here
- 
+# Loop until the user chooses to quit.
+while True:
+    # Check user input and prevent unexpected inputs by converting to upper-case
+    choice = input("(A)dd, (R)emove, (M)odify, (P)rint all, or (Q)uit? ").upper()
 
-   # Use a condition to handle adding a new student.
-   # Convert grade into integer
-   # Gather user input for "Enter the name of the student: "
-   # Check if student name already exists "Sorry, that student is already present."
-   # Gather user input for student grade "Enter the student's grade: "
-   # Validate input is in correct format or range, if not notify "Please enter grade as number 0-100"
-   # code here
- 
+    # Handle adding a new student
+    if choice == 'A':
+        name = input("Enter the name of the student: ")
 
-   # Handle removing a student if user inputs 'R'
-   # Check input for "What student do you want to remove? "
-   # use pop to remove key/value form grades
-   # see notes https://www.programiz.com/python-programming/methods/dictionary/pop
-   # Check if student doesn't exist - "Sorry, that student doesn't exist and couldn't be removed."
-   # code here
- 
+        if name in grades:
+            print("Sorry, that student is already present.")
+        else:
+            grade_input = input("Enter the student's grade: ")
+            try:
+                grade = int(grade_input)
+                if 0 <= grade <= 100:
+                    grades[name] = grade
+                else:
+                    print("Please enter grade as number 0-100")
+            except ValueError:
+                print("Please enter grade as number 0-100")
 
-   # Handle modifying a student grade if user inputs 'M'
-   # "Enter the name of the student to modify: "
-   # Convert grade into integer
-   # If student is in grades dictionary, show existing grade "The old grade is"
-   # Gather input for new grade "Enter the new grade: "
-   # If student doesn't exist "Sorry, that student doesn't exist and couldn't be modified."
-   # code here
- 
-   # Handle printing grade average as a string if user input is 'P'
-   # Use "The average grade is "
-   # Handle printing all of the student names with associated grade
-   # Display explictly as strings
-   # code here
- 
-      
+    # Handle removing a student
+    elif choice == 'R':
+        name = input("What student do you want to remove? ")
+        if name in grades:
+            grades.pop(name)
+        else:
+            print("Sorry, that student doesn't exist and couldn't be removed.")
+    # Handle modifying a student grade
+    elif choice == 'M':
+        name = input("Enter the name of the student to modify: ")
+        if name in grades:
+            print("The old grade is", grades[name])
+            grade_input = input("Enter the new grade: ")
+            try:
+                grade = int(grade_input)
+                if 0 <= grade <= 100:
+                    grades[name] = grade
+                else:
+                    print("Please enter grade as number 0-100")
+            except ValueError:
+                print("Please enter grade as number 0-100")
+        else:
+            print("Sorry, that student doesn't exist and couldn't be modified.")
 
-   # Handle the case for quiting if user inputs 'Q' "Goodbye!"
-   # code here
- 
+    # Handle printing grade average and all students
+    elif choice == 'P':
+        if grades:
+            average = sum(grades.values()) / len(grades)
+            print("The average grade is", average)
+            for student, grade in grades.items():
+                print(student, ":", grade)
+        else:
+            print("No students to display.")
 
-   # Handle the case of invalid input. "Sorry, that wasn't a valid choice."
-   # code here
+    # Handle quitting
+    elif choice == 'Q':
+        print("Goodbye!")
+        break
+
+    # Handle invalid input
+    else:
+        print("Sorry, that wasn't a valid choice.")
+
+
  
